53a68d4cd8759ede416db3dc9be5ba0c6c0d1ca3
Jaime Rios <jaimemadrigalrios@gmail.com>
2017-12-16 18:32:10 -0600

    Added controller validation.

--- server/controllers/authentication.js
+++ server/controllers/authentication.js
@@ -1,3 +1,41 @@
+const User = require('../models/user');
+
 exports.signup = (req, res, next) => {
-  res.send({ success: 'true' });
- }
+  console.log(req.body);
+
+  const email = req.body.email;
+  const password = req.body.password;
+
+  if (!email || !password) {
+    return res.status(422).send({ error: 'You most provide email address and password'});
+  }
+
+
+  //  See if user emai exists
+  User.findOne({ email }, (error, existingUser) => {
+    if (error) {
+      return next(error);
+    }
+
+    // If a user with email does exits, return error
+    if (existingUser) {
+      return res.status(422).send({ error: 'Email is in use' });
+    }
+
+    // If it does not exist, create user and save record
+    const user = new User({
+      email: email,
+      password: password
+    });
+
+
+    user.save((error) => {
+      if(error) {
+        return next(error);
+      }
+
+      res.json({ success: true })
+     })
+    // Respond to request
+  });
+}
