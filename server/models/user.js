034660de3aa0fc6ee8ecb1e685122409c7160f70
Jaime Rios <jaimemadrigalrios@gmail.com>
2017-12-16 18:59:46 -0600

    Added password encription

--- server/models/user.js
+++ server/models/user.js
@@ -1,5 +1,6 @@
 const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
+const bcrypt = require('bcrypt-nodejs');
 
 
 // Define model
@@ -11,6 +12,31 @@ const userSchema = new Schema({
   },
   password: String,
 });
+
+// On save hook, encrypt password
+// Before saving a model, run this function
+userSchema.pre('save', function(next) {
+
+  // get access ti user model
+  const user = this;
+
+  // generate a salt, then run callback
+  bcrypt.genSalt(10, (err, salt) => {
+    if(err) {
+      return next(err);
+    }
+    // hash our password using the salt
+    bcrypt.hash(user.password, salt, null, (err, hash) => {
+      if(err) {
+        return next(err);
+      }
+      // overwrite plain text password with encrypted password
+      user.password = hash;
+      next();
+    });
+  });
+});
+
 
 // Create model Class
 const ModelClass = mongoose.model('user', userSchema);
