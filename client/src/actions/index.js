e6466b820106c9e5affe1bc6faec2b74f13e0a83
Jaime Rios <jaimemadrigalrios@gmail.com>
2017-12-17 21:05:41 -0600

    Installed axios.

--- client/src/actions/index.js
+++ client/src/actions/index.js
@@ -1,18 +1,23 @@
+const ROOT_URL = 'http://localhost:3090';
+
 export const signinUser = ({ emai, password}) => {
-  // Submit email/password to server
+  return (dispatch) => {
+    dispatch({ type: });
+    // Submit email/password to server
 
 
-  // If request is good...
-  // -Update state to indicate user is authenticated
-  // - Save the JWT token
-  // -redirect to the route '/feature'
+    // If request is good...
+    // -Update state to indicate user is authenticated
+    // - Save the JWT token
+    // -redirect to the route '/feature'
 
 
-  // If request is bad..
-  // -Show an error to the user
+    // If request is bad..
+    // -Show an error to the user
 
-  return {
-    type: type,
-    payload: action.payload
-  }
+    // return {
+    //   type: type,
+    //   payload: action.payload
+    // }
+   }
 };
