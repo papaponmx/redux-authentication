1c9ea78a5236f8e304daedb6c780152c597ae96f
Jaime Rios <jaimemadrigalrios@gmail.com>
2017-12-17 20:14:57 -0600

    Added action creator.

---
+++ client/src/actions/index.js
@@ -0,0 +1,18 @@
+export const signinUser = ({ emai, password}) => {
+  // Submit email/password to server
+
+
+  // If request is good...
+  // -Update state to indicate user is authenticated
+  // - Save the JWT token
+  // -redirect to the route '/feature'
+
+
+  // If request is bad..
+  // -Show an error to the user
+
+  return {
+    type: type,
+    payload: action.payload
+  }
+};
