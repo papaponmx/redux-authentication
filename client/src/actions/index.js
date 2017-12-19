1eac9c7f9f369427989afb9164353aaf920bbc77
Jaime Rios <jaimemadrigalrios@gmail.com>
2017-12-18 21:17:31 -0600

    Added redirect.

--- client/src/actions/index.js
+++ client/src/actions/index.js
@@ -1,24 +1,32 @@
 import axios from 'axios';
+import { browserHistory } from 'react-router';
 
 const ROOT_URL = 'http://localhost:3090';
 
 export function signinUser({ email, password }) {
   return function(dispatch) {
     // Submit email/password to server
-    axios.post(`${ROOT_URL}/signin`, { email, password });
+    axios.post(`${ROOT_URL}/signin`, { email, password })
+    .then( response => {
+      // If request is good...
+      // -Update state to indicate user is authenticated
+      // - Save the JWT token
 
-    // If request is good...
-    // -Update state to indicate user is authenticated
-    // - Save the JWT token
-    // -redirect to the route '/feature'
+
+      // -redirect to the route '/feature'
+      browserHistory.push('/feature');
+    })
+    .catch(() => {
+      // If request is bad..
+      // -Show an error to the user
+
+    });
 
 
-    // If request is bad..
-    // -Show an error to the user
 
     // return {
     //   type: type,
     //   payload: action.payload
     // }
-   }
+  }
 };
