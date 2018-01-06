060789d74709fa2e8e4c86a7b60c00f4a88a18ff
Jaime Rios <jaimemadrigalrios@gmail.com>
2018-01-05 21:44:09 -0600

    Added route links to header.

--- client/src/components/header.js
+++ client/src/components/header.js
@@ -1,28 +1,46 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
+import { Link } from 'react-router';
 
 export class Header extends Component {
+  renderLinks() {
+    if(this.props.authenticated) {
+      return (
+        <li className="nav-item">
+          <Link className="nav-link" to="/signout">Sign out</Link>
+        </li>
+      )
+    } else {
+      return [
+        <li key={1} className="nav-item">
+          <Link className="nav-link" to="/signin">Sign in</Link>
+        </li>,
+        <li key={2} className="nav-item">
+          <Link className="nav-link" to="/signup">Sign up</Link>
+        </li>
+      ]
+    }
+  }
 
   render() {
     return (
       <nav className="navbar navbar-light">
+        <Link to="/" className="navbar-brand">Redux Auth</Link>
         <ul className="na navbar-nav">
-          <li className="nav-item">
-            Sign in
-          </li>
+          {this.renderLinks()}
         </ul>
       </nav>
     )
   }
 }
 
-// const mapStateToProps = (state) => ({
-//
-// });
-//
-// const mapDispatchToProps = {
-//
-// }
+const mapStateToProps = (state) => ({
+  authenticated: state.auth.authenticated,
+});
 
-export default Header;
-// export default connect(mapStateToProps, mapDispatchToProps)(Header);
+const mapDispatchToProps = {
+
+}
+
+// export default Header;
+export default connect(mapStateToProps, mapDispatchToProps)(Header);
