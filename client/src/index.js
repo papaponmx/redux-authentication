bb4d1a16b375e64195b66f5bbd205261f781c704
Jaime Rios <jaimemadrigalrios@gmail.com>
2018-01-06 22:48:45 -0600

    Sign in when app starts if token is available.

--- client/src/index.js
+++ client/src/index.js
@@ -4,7 +4,7 @@ import { Provider } from 'react-redux';
 import { createStore, applyMiddleware } from 'redux';
 import { Router, Route, IndexRoute, browserHistory } from 'react-router';
 import reduxThunk from 'redux-thunk';
-
+import { AUTH_USER } from './actions/types';
 
 import App from './components/app';
 import Feature from './components/feature';
@@ -16,9 +16,15 @@ import Welcome from './components/welcome';
 import reducers from './reducers';
 
 const createStoreWithMiddleware = applyMiddleware(reduxThunk)(createStore);
+const store = createStoreWithMiddleware(reducers);
+const token = localStorage.getItem('token');
+
+if (token) {
+  store.dispatch({ type: AUTH_USER });
+}
 
 ReactDOM.render(
-  <Provider store={createStoreWithMiddleware(reducers)}>
+  <Provider store={store}>
     <Router history={browserHistory}>
       <Route path="/" component={App} >
         <IndexRoute component={Welcome} />
