95edb65fac76cb18e1ab816b4af8bdb4e0f24f19
Jaime Rios <jaimemadrigalrios@gmail.com>
2018-01-06 22:02:04 -0600

    Added feature component.

--- client/src/index.js
+++ client/src/index.js
@@ -7,6 +7,7 @@ import reduxThunk from 'redux-thunk';
 
 
 import App from './components/app';
+import Feature from './components/feature';
 import Signin from './components/auth/signin';
 import Signout from './components/auth/signout';
 import Signup from './components/auth/signup';
@@ -18,6 +19,7 @@ ReactDOM.render(
   <Provider store={createStoreWithMiddleware(reducers)}>
     <Router history={browserHistory}>
       <Route path="/" component={App} >
+        <Route path="/feature" component={Feature} />
         <Route path="/signin" component={Signin} />
         <Route path="/signout" component={Signout} />
         <Route path="/signup" component={Signup} />
